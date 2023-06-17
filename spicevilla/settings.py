f7a69dfea59cb03cbe4387be9ce5cc099f5cbff3
Dhvani-intwala <>
2023-06-17 11:50:19 +0000

    Create a environment variables

--- spicevilla/settings.py
+++ spicevilla/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+if os.path.isfile('env.py'):
+    import env
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g1cxkfdnoy#9_z=70uk80i4j49x4$n#=6pqbb+@r(t+n1rjsjp'
+SECRET_KEY = 'e$^r!fxokj2$hg21ou)=r3t)hu0qfzor12))99t7xv)0#1lc!6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,6 +83,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 

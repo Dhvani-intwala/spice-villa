bfd277c48c81beb5bea8c6636830a0598a0f8e69
Dhvani-intwala <>
2023-06-19 08:56:57 +0000

    Add class booking in models file

--- booking/models.py
+++ booking/models.py
@@ -5,13 +5,14 @@ Models for Booking App
 """
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.conf import settings
 
 
 # Create your models here.
 
 class Table(models.Model):
     """
-    Model for teble object
+    Model for table object
     """
     code = models.CharField(max_length=10, unique=True)
     table_free_img = CloudinaryField('free_image')
@@ -20,3 +21,30 @@ class Table(models.Model):
 
     def __str__(self):
         return self.code
+
+    class Booking(models.Model):
+        """
+        Model for booking object
+        """
+
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    table = models.ForeignKey(Table, on_delete=models.CASCADE, to_field='code',
+                              blank=True)
+    customer_full_name = models.CharField(max_length=200,
+                                          blank=True)
+    customer_email = models.EmailField(max_length=200, blank=True)
+    created_on = models.DateTimeField(blank=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        to_field='email', blank=True)
+
+    class Meta:
+        ordering = ['data', 'start_time']
+
+    class BookingQuery(models.Model):
+        """
+        Model for booking query from
+        """
+        data = models.DateField()

87ddd36d1df5acf9ab644057834e91fa58fe620f
Dhvani Intwala <intdhvani2627@gmail.com>
2023-06-19 19:14:27 +0000

    Create a booking , meta, bookingquery class in models file

--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,6 @@
 """
-Booking App - models
+Booking App - Models
+----------------
 Models for Booking App
 
 """
@@ -7,8 +8,6 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 from django.conf import settings
 
-
-# Create your models here.
 
 class Table(models.Model):
     """
@@ -22,11 +21,11 @@ class Table(models.Model):
     def __str__(self):
         return self.code
 
-    class Booking(models.Model):
-        """
-        Model for booking object
-        """
 
+class Booking(models.Model):
+    """
+    Model for booking object
+    """
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
@@ -37,14 +36,18 @@ class Table(models.Model):
     customer_email = models.EmailField(max_length=200, blank=True)
     created_on = models.DateTimeField(blank=True)
     created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-        to_field='email', blank=True)
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ['data', 'start_time']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['created_by'], name='unique_booking_created_by')
+        ]
+        ordering = ['date', 'start_time']
 
-    class BookingQuery(models.Model):
-        """
-        Model for booking query from
-        """
-        data = models.DateField()
+
+class BookingQuery(models.Model):
+    """
+    Model for booking query form
+    """
+    date = models.DateField()

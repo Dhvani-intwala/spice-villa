80df7975600d6ada772ccb2ebafe41ec115a6253
Dhvani Intwala <intdhvani2627@gmail.com>
2023-06-23 12:22:23 +0000

    Create menu database diagram

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,24 @@
+"""
+Menu App - Models
+----------------
+Models for Menu App.
+"""
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.conf import settings
 
-# Create your models here.
+
+class Meal(models.Model):
+    """Model for Meal object"""
+    name = models.CharField(max_length=100, unique=True)
+    cover = CloudinaryField('image')
+    price = models.FloatField()
+    ingredients = models.CharField(max_length=1000)
+
+
+class Favourite(models.Model):
+    """Model for Favourite object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        to_field='email', blank=True)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, blank=True)

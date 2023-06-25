6bb89a4e53f49dbead690b15a00bd35243601fab
Dhvani Intwala <intdhvani2627@gmail.com>
2023-06-25 19:32:36 +0000

    Add a allowed hosts herokuapp link

--- spicevilla/settings.py
+++ spicevilla/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = 'e$^r!fxokj2$hg21ou)=r3t)hu0qfzor12))99t7xv)0#1lc!6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['spice-villa.herokuapp.com', 'localhost',
-                 '8000-dhvani-intwala-spice-vil-6d7504h6ef.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['spice-villa-c8a666e63481.herokuapp.com', 'localhost']
 
 
 # Application definition

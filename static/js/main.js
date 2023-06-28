07c61551c1b53da9ff2bff22bc90970cee1467ba
Dhvani Intwala <intdhvani2627@gmail.com>
2023-06-28 19:49:03 +0000

    Add chefs setion and apply style

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,50 @@
-(function () {
-    "use strict";
+// (function () {
+//     "use strict";
 
-    /**
-     * Easy selector helper function
-     */
-    new window().init();
+//     /**
+//      * Easy selector helper function
+//      */
+//     new window().init();
 
-    // couter-up
+//     // couter-up
 
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
+//     $('#temp').counterUp({
+//         delay: 10,
+//         time: 2000
+//     });
 
-})(jQuery);
+// })(jQuery);
+
+// menu functions
+
+// (function () {
+//     // Load the script
+//     const script = document.createElement("script");
+//     script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+//     script.type = 'text/javascript';
+//     script.addEventListener('load', () => {
+//         console.log(`jQuery ${$.fn.jquery} has been loaded successfully!`);
+//         // use jQuery below
+//     });
+//     document.head.appendChild(script);
+// })();
+jQuery(document).ready(function ($) {
+    if ($('.counter').counterUp) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000,
+        });
+    }
+});
+function startersAll() {
+    document.getElementById('allfood').className = 'd-none';
+    document.getElementById('starter').classList.remove("d-none");
+
+    console.log('here');
+}
+
+// document.getElementById('startersAll').addEventListener('click', () => {
+//     document.getElementById('allfood').className = 'd-none';
+//     document.getElementById('starter').classList.remove("d-none");
+//     console.log('here');
+// });

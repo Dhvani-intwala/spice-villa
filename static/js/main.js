cc89363764243d5af478be2aa2b47b6c4d5c8541
Dhvani Intwala <intdhvani2627@gmail.com>
2023-07-04 16:24:35 +0000

    Add alert messages disappear function

--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,5 @@
-// (function () {
-//     "use strict";
 
-//     /**
-//      * Easy selector helper function
-//      */
-//     new window().init();
 
-//     // couter-up
-
-//     $('#temp').counterUp({
-//         delay: 10,
-//         time: 2000
-//     });
-
-// })(jQuery);
-
-// menu functions
-
-// (function () {
-//     // Load the script
-//     const script = document.createElement("script");
-//     script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
-//     script.type = 'text/javascript';
-//     script.addEventListener('load', () => {
-//         console.log(`jQuery ${$.fn.jquery} has been loaded successfully!`);
-//         // use jQuery below
-//     });
-//     document.head.appendChild(script);
-// })();
 jQuery(document).ready(function ($) {
     if ($('.counter').counterUp) {
         $('.counter').counterUp({

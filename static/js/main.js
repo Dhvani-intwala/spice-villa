aa460fc5e6953926278f2c75b8fe5a35a65888bf
Dhvani Intwala <intdhvani2627@gmail.com>
2023-07-05 12:25:01 +0000

    Create mybooking.html file

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,23 @@
 
-    // back to top button
-window.onscroll = function() {
-  showBackToTopButton();
+// back to top button
+window.onscroll = function () {
+    showBackToTopButton();
 };
 
 function showBackToTopButton() {
-  var backToTopBtn = document.getElementById("backToTopBtn");
+    var backToTopBtn = document.getElementById("backToTopBtn");
 
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    backToTopBtn.style.display = "block";
-  } else {
-    backToTopBtn.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
 }
 
 // Function to scroll back to the top of the page
 function scrollToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 
@@ -72,36 +72,36 @@ function specialtyAll() {
 
 // testimonial slider 
 
- let slideIndex = 0;
-    showSlides();
- function showSlides() {
-     let i = 1;
-     let slides = document.getElementsByClassName('item');
-     for (i = 0; i < slides.length; i++) {
-         slides[i].style.display = "none";
-     }
-     slideIndex++;
-     if (slideIndex > slides.length) { slideIndex = 1; }
-     slides[slideIndex - 1].style.display = "block";
-     slides[slideIndex - 1].classList.remove("animate__slideOutLeft");
-     slides[slideIndex - 1].classList.add("animate_slideInRight");
-     slides[slideIndex].classList.remove('animate_slideInRight');
-     slideIndex[slideIndex].classlist.add('animate_slideOutLeft');
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+    let i = 1;
+    let slides = document.getElementsByClassName('item');
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].classList.remove("animate__slideOutLeft");
+    slides[slideIndex - 1].classList.add("animate_slideInRight");
+    slides[slideIndex].classList.remove('animate_slideInRight');
+    slideIndex[slideIndex].classlist.add('animate_slideOutLeft');
 
 
 
-     setTimeout(showSlides, 8000);
+    setTimeout(showSlides, 8000);
 }
 
-$(document).ready(function(){
-  $('.testimonial-slider').slick({
-    dots: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  });
-});
+// $(document).ready(function(){
+//   $('.testimonial-slider').slick({
+//     dots: true,
+//     arrows: false,
+//     autoplay: true,
+//     autoplaySpeed: 3000,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1
+//   });
+// });

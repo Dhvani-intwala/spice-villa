151f3e631459f0268138c19501cd92e22edaf337
Dhvani Intwala <intdhvani2627@gmail.com>
2023-07-05 09:33:59 +0000

    Add message alter bar style

--- static/js/main.js
+++ static/js/main.js
@@ -70,23 +70,38 @@ function specialtyAll() {
     document.getElementById('starter').className = 'd-none';
 }
 
-let slideIndex = 0;
-showSlides();
-function showSlides() {
-    let i = 1;
-    let slides = document.getElementsByClassName('item');
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1; }
-    slides[slideIndex - 1].style.display = "block";
-    slides[slideIndex - 1].classList.remove("animate__slideOutLeft");
-    slides[slideIndex - 1].classList.add("animate_slideInRight");
-    slides[slideIndex].classList.remove('animate_slideInRight');
-    slideIndex[slideIndex].classlist.add('animate_slideOutLeft');
+// testimonial slider 
+
+ let slideIndex = 0;
+    showSlides();
+ function showSlides() {
+     let i = 1;
+     let slides = document.getElementsByClassName('item');
+     for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+     }
+     slideIndex++;
+     if (slideIndex > slides.length) { slideIndex = 1; }
+     slides[slideIndex - 1].style.display = "block";
+     slides[slideIndex - 1].classList.remove("animate__slideOutLeft");
+     slides[slideIndex - 1].classList.add("animate_slideInRight");
+     slides[slideIndex].classList.remove('animate_slideInRight');
+     slideIndex[slideIndex].classlist.add('animate_slideOutLeft');
 
 
 
-    setTimeout(showSlides, 8000);
+     setTimeout(showSlides, 8000);
 }
+
+$(document).ready(function(){
+  $('.testimonial-slider').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+});

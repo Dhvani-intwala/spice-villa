7ede9b0c59b88a6261b3ce3e24f01dadb5d6b381
Dhvani Intwala <intdhvani2627@gmail.com>
2023-07-16 18:54:49 +0000

    Add a java script for active class

--- static/js/main.js
+++ static/js/main.js
@@ -87,3 +87,15 @@ function setPerson() {
     }
     document.getElementById('tablePersons').value = temp;
 }
+
+
+// Add active class to the current link
+var header = document.getElementById("navbarSupportedContent");
+var btns = header.getElementsByClassName("nav-link");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}

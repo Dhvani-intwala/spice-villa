8ac4cf0241e867631d71440ed5109b332cd4686c
Dhvani Intwala <intdhvani2627@gmail.com>
2023-07-20 19:54:44 +0000

    heroku profile edited gunicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: gunicorn spicevilla.wsgi
+web: gunicorn spicevilla.wsgi:application --log-file - --log-level debug
+python manage.py collectstatic --noinput
+manage.py migrate
--- booking/forms.py
+++ booking/forms.py
@@ -30,9 +30,8 @@ class BookingForm(forms.Form):
         attrs={'id': 'tableCode', 'class': 'form-select',
                'onChange': 'setPerson();'}),
         choices=(
-        ("A1", "A1"), ("A2", "A2"), ("A3", "A3"), ("B1", "B1"),
-        ("B2", "B2"), ("B3", "B3"), ("C1", "C1"), ("C2", "C2"),
-        ("C3", "C3")))
+        ("A1", "A1"), ("A2", "A2"), ("B1", "B1"),
+        ("B2", "B2"), ("B3", "B3"), ("C1", "C1"), ("C2", "C2")))
     customer_full_name = forms.CharField(widget=forms.TextInput(
         attrs={'id': 'fullName', 'class': 'form-control',
                'type': 'text', }), required=False)
